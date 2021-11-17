0efd023c189639f44d5e58ef309b9c9cdbae2819
Alexander <Sakeran@gmail.com>
2021-11-17 00:01:43 -0500

    Configure colors, fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,18 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}'],
+  mode: "jit",
+  purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}"],
+  theme: {
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      violet: "hsl(277, 64%, 61%)",
+      blue: {
+        800: "hsl(244, 38%, 16%)",
+        900: "hsl(233, 47%, 7%)",
+      },
+    },
+    fontFamily: {
+      lex: ["Lexend Deca", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+    },
+  },
 };
